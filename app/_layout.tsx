3c571924a38c904b745742eafd32797395137039
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-11-30 09:49:31 +0500

    add style.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text, View } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,12 +29,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={{
+      margin: "6px"
+    }}>
+      <Text style={{
+        color: "green",
+        fontSize: "30px",
+        fontWeight: "bold"
+      }}>
+        Hello React Native First App
+      </Text>
+    </View>
   );
 }
